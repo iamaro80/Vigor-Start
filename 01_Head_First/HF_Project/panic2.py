42390905a243ed29bf9ed2944aa9eda415851add
ibrahim <Ibrahim.Al-Maro@omnixjo.local>
2018-04-09 15:49:33 +0300

    end of page 100

---
+++ 01_Head_First/HF_Project/panic2.py
@@ -0,0 +1,27 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+
+new_phrase = ''.join(plist[1:3])
+new_phrase = new_phrase + ''.join([plist[5],plist[4],plist[7],plist[6]])
+print(new_phrase)
+print(plist)
+
+
+
+
+# for i in range(4):
+#     plist.pop()
+
+# plist.pop(0)
+# plist.remove("'")
+# print(plist)
+# plist.extend([plist.pop(),plist.pop()])
+# print(plist)
+# plist.insert(2,plist.pop(3))
+
+# print(plist)
+
+# new_phrase = ''.join(plist)
+# print(plist)
+# print(new_phrase)
+

bab88f5330f0df7a13e9beff7a3abde1aa17ca0d
ibrahim <Ibrahim.Al-Maro@omnixjo.local>
2018-04-11 17:02:13 +0300

    still thinking

--- 01_Head_First/Shopping List Project/V1.0/main.py
+++ 01_Head_First/Shopping List Project/V1.0/main.py
@@ -1,6 +1,7 @@
 # shopping_lists=['list1','list2']
 # shopping_lists= {'id':0,'name':'list1','id':1,'name':'list2'}
 
+# think of the combination of list and dict (try sorted and setdefault methods)
 units=['kg','liter']
 
 item = {'name':'rice',

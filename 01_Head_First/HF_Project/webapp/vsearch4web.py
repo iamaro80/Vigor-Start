431e139866fd845bec26d3d94eef8d7beebedb15
= <ibrahimmaro@gmail.com>
2018-04-21 04:00:45 +0300

    Page 238

--- 01_Head_First/HF_Project/webapp/vsearch4web.py
+++ 01_Head_First/HF_Project/webapp/vsearch4web.py
@@ -1,23 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from vsearch import search4letters
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello from Flask!'
+# @app.route('/')
+# def hello() -> '302':
+#     return redirect('/entry')
 
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('abcdefghijklmnopqrstvuwxyz', 'ald8215'))
+    title = 'Here are your results:'
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    results = str(search4letters(phrase, letters)) 
+    return render_template(
+        'results.html', 
+        the_title=title,
+        the_phrase=phrase,
+        the_letters=letters,
+        the_results=results)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() ->'html':
     return render_template(
         'entry.html', the_title='Welcome to search4letters on the web!')
 
+# units = {'kg', 'liter', 'box', 'packet', 'unit', }
+# units = set()
+# for x in range(1980, 2019, 1):
+#     units.add(x)
+# print(units)
 
-app.run()
+# @app.route('/list')
+# def load_list_items() ->'html':
+#     title = 'En Units list'
+#     return render_template('list.html', the_title=title, the_units=sorted(units))
+
+app.run(debug=True)
+
--- 01_Head_First/Shopping List Project/V1.0/main.py
+++ 01_Head_First/Shopping List Project/V1.0/main.py
@@ -2,12 +2,38 @@
 # shopping_lists= {'id':0,'name':'list1','id':1,'name':'list2'}
 
 # think of the combination of list and dict (try sorted and setdefault methods)
-#units=['kg','liter']
+# units=['kg','liter']
 units = {}
-units['en'] = ('kg','lit','box','packet')
-units['ar'] = ('كيلو','لتر','صندوق','باكيت')
-# print(units['en'])
-print(units)
+units['en'] = {'kg', 'liter', 'box', 'packet'}
+units['ar'] = {'كيلو', 'لتر', 'صندوق', 'باكيت'}
+units['fr'] = {'kg', 'litre', 'boîte', 'paquet'}
+
+
+def call_unit_by_lang(lang: str='en') ->set:
+    '''Return a set of units for specific language'''
+    return set(units[lang])
+
+x = call_unit_by_lang('fr')
+print(sorted(x))
+
+
+def add_unit_by_lang(lang: str, unit: str) ->str:
+    '''Add unit to specific language units'''
+    return units[lang].add(unit)
+
+
+add_unit_by_lang('fr', 'unité')
+print(call_unit_by_lang('fr'))
+    
+
+# print(call_unit_by_lang('en'))
+# print(add_unit_by_lang('en', 'packet'))
+# print(units['en'], 'add new unit to en')
+# lang = call_unit_by_lang('en')
+# print(type(lang), 'type of lang')
+# print(lang, 'lang elements')
+# #lang = units['en'].remove('packet')
+# print(units['en'], 'remove unit from unit')
 
 # item = {'name':'rice',
 #         'amount':5,

c11f80da902899b62bbe1d334969ee1f9cdd60d8
ibrahim <Ibrahim.Al-Maro@omnixjo.local>
2018-04-22 16:56:27 +0300

    Page 245

--- 01_Head_First/HF_Project/webapp/vsearch4web.py
+++ 01_Head_First/HF_Project/webapp/vsearch4web.py
@@ -9,25 +9,31 @@ app = Flask(__name__)
 #     return redirect('/entry')
 
 
+@app.route('/')
+def start() ->'html':
+    return render_template('start.html')
+
+@app.route('/entry')
+def entry_page() ->'html':
+    title = 'Welcome to search4letters on the web!'
+    return render_template(
+        'entry.html', 
+        the_title=title)
+
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
     title = 'Here are your results:'
     phrase = request.form['phrase']
     letters = request.form['letters']
-    results = str(search4letters(phrase, letters)) 
+    results = str(search4letters(phrase, letters))
     return render_template(
-        'results.html', 
+        'results.html',
         the_title=title,
         the_phrase=phrase,
         the_letters=letters,
         the_results=results)
 
-
-@app.route('/')
-@app.route('/entry')
-def entry_page() ->'html':
-    return render_template(
-        'entry.html', the_title='Welcome to search4letters on the web!')
 
 # units = {'kg', 'liter', 'box', 'packet', 'unit', }
 # units = set()
@@ -38,7 +44,10 @@ def entry_page() ->'html':
 # @app.route('/list')
 # def load_list_items() ->'html':
 #     title = 'En Units list'
-#     return render_template('list.html', the_title=title, the_units=sorted(units))
+#     return render_template(
+#           'list.html',
+#           the_title=title,
+#           the_units=sorted(units))
 
-app.run(debug=True)
-
+if __name__ == '__main__':
+    app.run(debug=True)
